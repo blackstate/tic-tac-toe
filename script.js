0bbc4b296643ba350841b8416d3d3eab60b64088
blackstate <josemiguelmneyra@gmail.com>
2020-05-24 17:02:48 +0800

    added win checker

--- script.js
+++ script.js
@@ -1,13 +1,47 @@
 const gameBoard = (function () {
     const _board = document.querySelector("div#gameBoard");
     const _boardArr = [
-        [1,undefined,undefined],
-        [undefined,1,undefined],
-        [undefined,undefined,1]
+        [undefined,undefined,undefined],
+        [undefined,undefined,undefined],
+        [undefined,undefined,undefined]
     ];
     let currentSym = 1;
     let currentMarker = "x";
-   
+    let a, b, c;
+    let winningCombo = []
+
+    
+    function _checkWin () {
+        winningCombo = [
+            [_boardArr[0][0], _boardArr[0][1], _boardArr[0][2]],
+            [_boardArr[1][0], _boardArr[1][1], _boardArr[1][2]],
+            [_boardArr[2][0], _boardArr[2][1], _boardArr[2][2]],
+    
+            [_boardArr[0][0], _boardArr[1][0], _boardArr[2][0]],
+            [_boardArr[0][1], _boardArr[1][1], _boardArr[2][1]],
+            [_boardArr[0][2], _boardArr[1][2], _boardArr[2][2]],
+    
+            [_boardArr[0][0], _boardArr[1][1], _boardArr[2][2]],
+            [_boardArr[0][2], _boardArr[1][1], _boardArr[2][0]]
+        ]
+        
+
+        for (i = 0; i < 8; i++) {
+            a = winningCombo[i][0];
+            b = winningCombo[i][1];
+            c = winningCombo[i][2];
+
+            if (a == undefined || b == undefined || c == undefined){
+                continue;
+            }
+
+            if (a == b && a == c && b == c) {
+                console.log(_boardArr);
+                console.log('winner');
+            }
+        }
+    }
+
     function _addTic() {
         if (this.innerHTML != "")
             return;
@@ -43,29 +77,32 @@ const gameBoard = (function () {
         }
     }
 
-    function updateBoard () {
+    function _updateBoard () {
         let position = this;
+
         if (position >= 0 && position < 3) {
-            _boardArr[0][position] = currentSym;
+            if (_boardArr[0][position] == undefined) 
+                _boardArr[0][position] = currentSym;
         }
         else if (position >= 3 && position < 6) {
-            _boardArr[1][(position-3)] = currentSym;
+            if (_boardArr[1][position-3] == undefined)
+                _boardArr[1][(position-3)] = currentSym;
         }
-        else  {
-            _boardArr[2][(position-6)] = currentSym;
+        else if (position >= 6) {
+            if (_boardArr[2][position-6] == undefined)
+                _boardArr[2][(position-6)] = currentSym;
         }
-        console.log(_boardArr);
         _updateSymbol ();
     }
 
-    const addCellEvent = () => {
+    function addCellEvent () {
         let boardCell = Array.from(document.querySelectorAll(".cell"));
         boardCell.forEach((cell, i) => {
             cell.addEventListener('click', _addTic.bind(cell));
-            cell.addEventListener('click', updateBoard.bind(i))
+            cell.addEventListener('click', _updateBoard.bind(i))
+            cell.addEventListener('click', _checkWin)
         })
     }
-
 
     function render () {
         let cell;
